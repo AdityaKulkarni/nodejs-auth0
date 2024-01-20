2171cf4c20c48188d4dec8290d1ae2c1c4dda84c
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2024-01-20 14:39:08 -0800

    Final commit

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const jwtCheck = auth({
 })
 
 const app = express()
-app.use(jwtCheck)
+//app.use(jwtCheck)
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
